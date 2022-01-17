7c61bf1ed00d07d58675c52daf1f18ce5183699c
unknown <deepakroy0696@gmail.com>
2022-01-17 12:07:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,90 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useState } from 'react';
+import Vendor from './Componenets/Vendor';
+import Tabs from './Componenets/Tabs';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
-  const [num, setNum] = useState('');
-  const [det, setDet] = useState([]);
-
-  const submit = (e) => {
-    e.preventDefault();
-    if(!num){
-      alert('field cannot be empty');
-    }else{
-      fetch(
-        'https://jsonplaceholder.typicode.com/posts',
-        {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(num),
-        }
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response.value);
-          setDet(response.value);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      console.log(num);
-      setNum('');
-    }
-  }
-
   return (
     <>
-    <div className='container col-md-4 shadow card'>
-    <center><h1>SAP Vendor</h1></center>
-    {/* <label className="form-label mt-5"><h6>vendor Number</h6></label> */}
-    <input type="text" className="form-control mt-5 mb-3" placeholder='Enter vendor Number...' value={num} onChange={(e) =>{setNum(e.target.value)}}/>
-    <center><button type='submit' className='btn btn-primary mb-3' onClick={submit}>Submit</button></center>
-    </div> 
+      <Router >
+        <ul className="container nav justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="/tabs">Tabs</a>
+          </li>          
+        </ul>
+        <Routes>
+          <Route exact path='/' element={<Vendor />} />
+          <Route exact path='/tabs' element={<Tabs />} />
+        </Routes>
+      </Router>
 
-    <div className='container card shadow'>
-    <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">VENDOR</th>
-      <th scope="col">NAME</th>
-      <th scope="col">CITY</th>
-      <th scope="col">POBX_PCD</th>
-      <th scope="col">POSTL_CODE</th>
-      <th scope="col">REGION</th>
-      <th scope="col">STREET</th>
-      <th scope="col">COUNTRY</th>
-      <th scope="col">COUNTRY_ISO</th>
-      <th scope="col">LANGU</th>
-      <th scope="col">LANGU_ISO</th>
-      <th scope="col">FORMOFADDR</th>
-    </tr>
-  </thead>
-  <tbody>
-  {det.map((value,index)=>(
-    <tr key={index}>
-      <th scope="row">{value.VENDOR}</th>
-      <td>{value.NAME}</td>
-      <td>{value.CITY}</td>
-      <td>{value.POBX_PCD}</td>
-      <td>{value.POSTL_CODE}</td>
-      <td>{value.REGION}</td>
-      <td>{value.STREET}</td> 
-      <td>{value.COUNTRY}</td>
-      <td>{value.COUNTRY_ISO}</td>
-      <td>{value.LANGU}</td>
-      <td>{value.LANGU_ISO}</td>
-      <td>{value.FORMOFADDR}</td>            
-    </tr>))}
-  </tbody>
-</table>
-    </div>
     </>
   );
 }
-
 export default App;
